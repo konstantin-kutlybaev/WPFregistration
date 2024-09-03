7010b04b2fb946f9225ce4a2890b872da033f9e3
unknown <kutlybaev@gmail.com>
2024-09-03 21:29:52 +0400

    регистрация и авторизация

---
+++ WpfRegistration/WpfRegistration/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfRegistration
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void AuthorizationButtonClick(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(password))
+            {
+                MessageTextBlock.Text = "Пожалуйста, заполните все поля.";
+                return;
+            }
+            else
+            {
+                MessageTextBlock.Text = "";
+                return;
+            }
+        }
+        private void OpenRegisrationButtonClick(object sender, RoutedEventArgs e)
+        {
+            Regisration regisration = new Regisration();
+            regisration.Show();
+            this.Close();
+        }
+    }
+}
---
+++ WpfRegistration/WpfRegistration/Regisration.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfRegistration
+{
+    /// <summary>
+    /// Логика взаимодействия для Regisration.xaml
+    /// </summary>
+    public partial class Regisration : Window
+    {
+        public Regisration()
+        {
+            InitializeComponent();
+        }
+        private void RegisrationButtonClick(object sender, RoutedEventArgs e)
+        {
+            string name = nameTextBox.Text;
+            string username = UsernameTextBox.Text;
+            string passwordOne = PasswordBoxOne.Password;
+            string passwordTwo = PasswordBoxTwo.Password;
+
+            if (string.IsNullOrWhiteSpace(username) || string.IsNullOrWhiteSpace(passwordOne) || string.IsNullOrWhiteSpace(passwordTwo))
+            {
+                MessageTextBlock.Text = "Пожалуйста, заполните все поля.";
+                return;
+            }
+            else if (passwordOne != passwordTwo)
+            {
+                MessageTextBlock.Text = "Пароли не соотвествуют друг другу";
+                return;
+            }
+            else
+            {
+                MessageTextBlock.Text = "";
+                return;
+            }
+        }    
+    }
+}
